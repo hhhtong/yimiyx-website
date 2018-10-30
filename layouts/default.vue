68e4fcab576e3131170172100f17bd6f94a573ee
songyazhao <syazhao@foxmail.com>
2018-10-30 16:01:35 +0800

    Feature: baidu statistics

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,21 @@
     <nuxt/>
   </div>
 </template>
+
+<script>
+const hm = {
+  type: 'text/javascript',
+  innerHTML: `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement('script');
+        hm.src = 'https://hm.baidu.com/hm.js?49785f0478d709a92e396c4b7b706729';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(hm, s);
+      })();`
+}
+const script = process.env.NODE_ENV === 'production' ? [hm] : []
+export default {
+  head: { script, __dangerouslyDisableSanitizers: ['script'] }
+}
+</script>
